107926552f9756cb36cd2693ad405743b5b06f56
Simon Houdini <simonngumbau7@gmail.com>
2026-09-17 04:55:26 -0700

    added custom event fbq

--- routine.js
+++ routine.js
@@ -760,6 +760,8 @@ async function bulkOrder(product) {
 
 
 function sendOrder(shop, number) {
+
+    fbq('trackCustom', 'OrderOnWhatsApp')
     
     const shop_div = shop.parentElement
     const shop_name = shop_div.querySelector(".shop-name").textContent
