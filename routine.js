0a1dce5844f9cff7755fd3742bb0c81166120470
Simon Houdini <simonngumbau7@gmail.com>
2026-09-17 02:00:14 -0700

    track whatsapp order

--- routine.js
+++ routine.js
@@ -761,7 +761,7 @@ async function bulkOrder(product) {
 
 function sendOrder(shop, number) {
 
-    fbq('track', 'InitiateCheckout');
+    fbq('track', 'WhatsappOrder');
     
     const shop_div = shop.parentElement
     const shop_name = shop_div.querySelector(".shop-name").textContent
